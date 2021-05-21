ca6444ed48c80c9a43f61abbee6c1bfd289ee417
Kawa <victor.borruat@hotmail.com>
2021-05-21 17:11:55 +0200

    first commit

---
+++ filtering.py
@@ -0,0 +1,213 @@
+import os
+import pandas as pd
+import numpy as np
+from numpy import fft as fft
+import matplotlib.pyplot as plt
+
+# PATH GETTER ========================================
+def getpath():
+    dir_path = os.path.dirname(os.path.realpath("filtering.py"))
+    return dir_path
+
+
+# SHEET GETTER ========================================
+def getPatientSheetsConcat(path):
+    all_sheets = []
+    # fetch from files
+    excelFile = pd.ExcelFile(path+"/exports/000/eye0 all.xlsx")
+    sheets= list(range(1, len(excelFile.sheet_names)-1))
+    # creates list of files
+    all_numpys = []
+    for i in sheets:
+        f = pd.read_excel(excelFile, sheet_name= str(i),dtype={'pupil_timestamp': float,
+                                    'diameter': float})
+        n = f.to_numpy()
+        
+        all_sheets.append(f)
+        all_numpys.append(n)
+        
+    last = []
+    return all_numpys
+
+
+# HELPER FUNCTIONS ========================================
+
+def compute_baseline(pre, trigger_time):
+    #using Astrid's formula
+    return (np.mean(pre[0:30]))
+
+#-----------------------
+def fill_nan(array):
+    a = np.copy(array)
+    nan_idx = pd.isnull(a)
+    good_idx = np.logical_not(nan_idx)
+    good_data = a[good_idx]
+    interpolated = np.interp(nan_idx.nonzero()[0], 
+                             good_idx.nonzero()[0], 
+                             np.array(good_data, dtype='float64'))
+    b = a
+    b[nan_idx] = interpolated
+    return b
+
+#-----------------------
+def connect(a, b):
+    return np.concatenate((a, b))
+
+#-----------------------
+def highcut(array, hcut):
+    a = np.copy(array)
+    a = np.where(a > hcut, a, np.nan)
+    return fill_nan(a)
+
+#-----------------------
+def min_and_value(array):
+    m = np.argmin(array)
+    return m, array[m]
+
+# SHEET SPLITTER ========================================
+
+def splitprepost(sheets):
+    pre_triggers = []
+    post_triggers = []
+    triggers = []
+    for i in range(len(sheets)):
+        sheet = sheets[i] 
+        
+        if i %2 == 0:
+            post = sheets[i+1][:,1]
+            # fetches pre-trigger DataFrame
+            current_array = fill_nan(sheet[:,1])
+            #mean = np.repeat(np.mean(current_array), current_array.shape[0])
+            #pre_triggers.append(current_array)
+            
+            trigger_idx = np.where(sheets[0][:,2] == 'S')[0][0]
+            #print(trigger_idx)
+            
+            current_array = fill_nan(sheet[:,1])
+            
+            #mean = np.repeat(np.mean(current_array), current_array.shape[0])
+            pre_triggers.append(current_array[:trigger_idx])
+            post = np.append(current_array[trigger_idx:], post)
+            post_triggers.append(post)
+            triggers.append(trigger_idx)
+        
+    return np.copy(pre_triggers), np.copy(post_triggers), np.copy(triggers)
+
+# FOURIER FILTER ========================================
+
+def fourier_filter(array, fcut = 40, samples_to_add = 2000):
+    a = np.copy(array) # input array
+    #fcut = 100
+    #ZERO PADDING input signal 
+    samples_to_add = 2000
+    a = np.concatenate((np.ones(samples_to_add)*a[0], a, np.ones(samples_to_add)*a[-1]))
+    total_samples = a.shape[0]
+    # "resizing" if required for frequency domain plot
+    if total_samples %2 != 0:
+        total_samples -=1
+        a = a[:-1]
+
+    # first-order filter we are using
+    filt = np.concatenate((np.zeros(int(total_samples/2-fcut)), 
+                      np.linspace(0, 1, fcut), 
+                      np.linspace(1, 0, fcut),
+                      (np.zeros(int(total_samples/2-fcut)))))
+
+    # compute Fourier Transform and show frequency domain, shifts for correct plot
+    ft = np.fft.fft(a)
+    ftshifted = np.fft.fftshift(ft)
+
+
+    # make sure, should always pass though
+    assert(ftshifted.shape[0] == filt.shape[0])
+
+    result = (ftshifted*filt)
+    return fft.ifft(fft.fftshift(result))[samples_to_add:-samples_to_add].real
+
+
+
+# GRAPH MAKER =========================================================
+def plot_graph():
+    a = np.ones(100)
+    plt.plot(a, '--', label='baseline', color='gray')
+
+def make_graphs(save_path, patient_name, pre, post, trigger, hcut, constr_time, show_original):
+    if hcut == 0:
+        hcut = 40
+    if constr_time == 0:
+        constr_time = 400
+
+    assert(len(pre) == len(trigger))
+    assert(len(post) ==len(pre))
+
+    all_data =""
+
+    for i in range(len(post)):
+        post_ = highcut(np.copy(post[i]), hcut)
+        pre_ = np.copy(pre[i])
+
+
+        current_min = post_[0]
+        #process post_ with simple derivative filter 
+        for j in range(1, constr_time):
+            if post_[j] <= current_min and post_[j] != np.nan:
+                current_min = post_[j]
+                
+            if post_[j] > current_min:
+                post_[j] = np.nan
+            p = fill_nan(post_) # linear interpolation
+                
+        ft = fourier_filter(p, fcut = 150)
+        plt.figure(figsize=(10,5))
+        
+        baseline = np.ones(
+            pre[i].shape[0]+post[i].shape[0])*compute_baseline(fill_nan(pre[i]), trigger[i])    
+
+
+        # PLOTTING 
+
+        plt.plot(baseline, '--', label='baseline', color='gray')
+        
+        if(show_original == 1):
+            plt.plot(connect(pre_, np.copy(post[i])), label='original', color='green') #original graph
+            plt.plot(connect(pre_, np.copy(p)), label='filtered', color='blue') #original graph
+
+        #plt.plot(connect(pre_, post_), label='filtered', color='g')
+        plt.plot(connect(pre_, ft), label='processed', color='r')
+        
+        
+        
+        plt.plot(trigger[i], connect(pre_, ft)[trigger[i]], 'x',mew=2, ms=5, label='trigger', color='black')
+        
+        
+        mindex, min_value = min_and_value(ft[:400])
+        plt.plot(mindex, min_value, '+', label= 'min value', color='black', mew=5,ms=10)
+        plt.title(label=patient_name)
+        
+        plt.legend()
+
+        #saving image & making Data File
+        frames_to_min = (mindex-trigger[i])
+        time_to_min = (1/120)*(mindex-trigger[i])
+        
+        #print("Time to min from trigger: ~{:.2f} sec - aka frame number {}".format(time_to_min, frames_to_min))
+        all_data +="Trigger number [{}] : Time to min from trigger: ~{:.2f} sec \n".format(i+1, time_to_min)
+        
+        save_path_graphs = save_path+'/results/processed_'+str(i)
+        if not os.path.exists(save_path+'/results'):
+            os.mkdir(save_path+'/results')
+        plt.savefig(fname=save_path_graphs)
+        
+    #write data 
+    """ with open(save_path+"/data.txt", "w") as f:
+            f.write(all_data)
+            f.close() """
+    write_data(save_path, 'Time from Trigger to Maximal Contraction', all_data)
+
+def write_data(path, title, data):
+    with open(path+"/data.txt", "w") as f:
+            f.write(title + '\n')
+            f.write('\n')
+            f.write(data)
+            f.close()
+    
---
+++ gui.py
@@ -0,0 +1,118 @@
+from tkinter import *
+import os
+from matplotlib.pyplot import show
+
+from numpy.core.numeric import ones
+from filtering import *
+
+app = Tk()
+
+def makepath():
+    print(getpath() + '/{}/{}'.format(path_name.get(), folder_name.get()))
+    return getpath() + '/{}/{}'.format(path_name.get(), folder_name.get())
+
+def patient_sheets():
+    # return pre, post, triggers of the given patient
+    pre, post, trigger = splitprepost(getPatientSheetsConcat(makepath()))
+    print('fetched excel files')
+    return pre, post, trigger
+
+# computes all graphs for given patient
+def compute_graphs():
+    pre, post, trigger = patient_sheets()
+
+    # big if statement for conditions, could work on a better version
+    """ if (highcut.get() != 0):
+        if constr_time != 0:
+            make_graphs(makepath(), path_name, pre, post, trigger, highcut.get(), constr_time.get()*120)
+        else:
+         make_graphs(makepath(), path_name, pre, post, trigger, highcut.get())           
+    else:
+        if constr_time != 0:
+            make_graphs(makepath(), path_name, pre, post, trigger, constr_time.get()*120)
+        else: """
+    make_graphs(makepath(), path_name, pre, post, trigger, highcut.get(), constr_time.get()*120, show_original.get())
+    print("graphs done, check the file")
+
+def y():
+    print(highcut.get())
+
+app.title('Pupil Graph Analysis')
+app.geometry('800x400')
+
+
+# LEFT SIDE - GRAPH MAKER - COLUMNS 0 & 1
+
+#fetch path name
+path_name = StringVar()
+path_name_label = Label(app, text='Path to file:', font=('bold',14), pady = 20)
+path_name_label.grid(row = 0, column=0)
+
+#path entry
+path_entry = Entry(app, textvariable=path_name)
+path_entry.grid(row=0, column=1)
+
+#folder name
+folder_name = StringVar()
+folder_name_label = Label(app, text='Path to folder:', font=('bold',14), pady = 20)
+folder_name_label.grid(row = 1, column=0)
+
+#folder name entry
+folder_entry = Entry(app, textvariable=folder_name)
+folder_entry.grid(row=1, column=1)
+
+""" fetch_btn = Button(app, text='fetch data', width=12, command=patient_sheets)
+fetch_btn.grid(row=2, column=1) """
+opt_text = Label(app, text='Optional Settings', padx=20)
+opt_text.grid(row=2, column=0)
+
+#highcut selector
+highcut = IntVar()
+highcut_label = Label(app, text='Highcut: ', font=('bold',14), pady=20)
+highcut_label.grid(row = 3, column=0)
+highcut_entry = Entry(app, textvariable=highcut)
+highcut_entry.grid(row=3, column=1)
+
+# contraction time selector
+constr_time = IntVar()
+constr_time_label = Label(app, text=' Max. time to min[s]', font=('bold',14), pady=20)
+constr_time_label.grid(row = 4, column=0)
+constr_time_entry = Entry(app, textvariable=highcut)
+constr_time_entry.grid(row=4, column=1)
+
+# show original graph toggle
+show_original = IntVar()
+show_original_label = Checkbutton(app, text='show original graph', variable=show_original, onvalue=1, offvalue=0)
+show_original_label.grid(row = 5, column=0)
+
+
+
+compute_btn = Button(app,text='compute graphs', width=12, command=compute_graphs)
+compute_btn.grid(row=6, column=1)
+
+
+
+
+# RIGHT SIDE - STATISTICS
+stats = Label(app, text='Statistics', font=('bold', 14), padx=150)
+stats.grid(row = 0, column=2)
+
+area_06 = IntVar()
+area_06_label = Checkbutton(text='Area under curve [0s-6s]', variable=area_06, onvalue=1, offvalue=0)
+area_06_label.grid(row=1, column=2)
+
+area_630 = IntVar()
+area_630_label = Checkbutton(text='Area under curve [6s-30s]', variable=area_630, onvalue=1, offvalue=0)
+area_630_label.grid(row=2, column=2)
+
+latency = IntVar()
+latency_label = Checkbutton(text='Latency', variable=latency, onvalue=1, offvalue=0)
+latency_label.grid(row=3, column=2)
+
+velocity = IntVar()
+velocity_label = Checkbutton(text='Velocity', variable=area_06, onvalue=1, offvalue=0)
+velocity_label.grid(row=4, column=2)
+
+
+#runs app
+app.mainloop()
